539cd3ed56e936a590b46669c60a6db6dcc91d26
Alex Bazlinton <abazlinton@gmail.com>
2016-10-12 14:16:57 +0100

    runner created

---
+++ db/sql_runner.rb
@@ -0,0 +1,20 @@
+require 'pg'
+
+class SqlRunner
+
+  def self.run(sql)
+  begin 
+    db = PG.connect(dbname: 'pet_store_chain', name: 'localhost')
+    result = db.exec(sql)
+
+  ensure
+    db.close
+  end
+    return result
+  end
+
+
+
+
+
+end
